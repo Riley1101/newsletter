30490c0ab52f57440e88abc830164f52ced7cb8d
Riley1101 <www.riley.s.eileen@gmail.com>
2023-05-09 17:00:48 -0400

    added flexible port in testing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use newsletter::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // Bubble up the io::Error if we failed to bind the address
-    // Otherwise call .await on our Server
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8080");
+    match listener {
+        Ok(listener) => {
+            run(listener)?.await
+        },
+        Err(_) => todo!(),
+    }
 }
