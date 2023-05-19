4b2aafe0ae21a24ce54136ccec06520907c72943
Riley1101 <www.riley.s.eileen@gmail.com>
2023-05-18 22:27:51 -0400

    addded try forms and better seperation of concerns in subscribe method

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -37,19 +37,21 @@ pub async fn insert_subscriber(pool:&PgPool, new_subscriber: &NewSubscriber) -> 
     Ok(())
 }
 
+impl TryFrom<FormData> for NewSubscriber{
+    type Error = String;
+    fn try_from(value: FormData) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(value.name)?;
+        let email = SubscriberEmail::parse(value.email)?;
+        Ok(Self{name, email})
+    }
+}
+
 pub async fn subscribe(form:web::Form<FormData>,pool:web::Data<PgPool>) ->HttpResponse{
-    let email = match SubscriberEmail::parse(form.0.email) {
-        Ok(email) =>email,
+    let new_subscriber = match form.0.try_into() {
+        Ok(subscriber) => subscriber,
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
-    let name= match SubscriberName::parse(form.0.name) {
-        Ok(name) =>name,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-    let new_subscriber = NewSubscriber{
-        name,
-        email
-    };
+        
     match insert_subscriber(&pool, &new_subscriber).await {
        Ok(_) => HttpResponse::Ok().finish(),
        Err(_) => HttpResponse::InternalServerError().finish(),
